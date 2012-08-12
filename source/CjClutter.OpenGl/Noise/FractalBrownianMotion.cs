fa5dcd1fc21cb1715b033c9324cc0217a144da80
Chris Jansson <chris.janssom@gmail.com>
2012-08-12 17:22:46 +0200

    Use INoiseGenerator in fbm
    Changed ctor parameter to INoiseGenerator for fbm, also implemented
    interface on fbm as well since it's just a different way of generating
    noise

--- source/CjClutter.OpenGl/Noise/FractalBrownianMotion.cs
+++ source/CjClutter.OpenGl/Noise/FractalBrownianMotion.cs
@@ -1,17 +1,35 @@
 ﻿namespace CjClutter.OpenGl.Noise
 {
-    public class FractalBrownianMotion
+    public class FractalBrownianMotion : INoiseGenerator
     {
         private const double Lacunarity = 2.0; //1.8715 or 2.1042 can help reduce artifacts in some algorithms
         private const double Gain = 0.5; //Generally 1/Lacunarity
 
-        private readonly ImprovedPerlinNoise _noiseGenerator;
+        private readonly INoiseGenerator _noiseGenerator;
         private readonly FractalBrownianMotionSettings _settings;
 
-        public FractalBrownianMotion(ImprovedPerlinNoise noiseGenerator, FractalBrownianMotionSettings settings)
+        public FractalBrownianMotion(INoiseGenerator noiseGenerator, FractalBrownianMotionSettings settings)
         {
             _settings = settings;
             _noiseGenerator = noiseGenerator;
+        }
+
+        public double Noise(double x, double y)
+        {
+            var frequency = _settings.Frequency;
+            var amplitude = _settings.Amplitude;
+            var octaves = _settings.Octaves;
+
+            var result = 0.0;
+
+            for (var i = 0; i < octaves; i++)
+            {
+                result += _noiseGenerator.Noise(x * frequency, y * frequency) * amplitude;
+                frequency *= Lacunarity;
+                amplitude *= Gain;
+            }
+
+            return result;
         }
 
         public double Noise(double x, double y, double z)
--- source/CjClutter.OpenGl/Noise/INoiseGenerator.cs
+++ source/CjClutter.OpenGl/Noise/INoiseGenerator.cs
@@ -1,6 +1,6 @@
 ﻿namespace CjClutter.OpenGl.Noise
 {
-    interface INoiseGenerator
+    public interface INoiseGenerator
     {
         double Noise(double x, double y);
         double Noise(double x, double y, double z);
