48d43bbf94cec781ff845271d5fb09f3989b3d77
Chris Jansson <chris.janssom@gmail.com>
2012-08-12 17:11:26 +0200

    Another simple test run to check that the noise is working properly

--- source/CjClutter.ObjLoader.Test/OpenGlTestProgram.cs
+++ source/CjClutter.ObjLoader.Test/OpenGlTestProgram.cs
@@ -12,34 +12,9 @@ namespace ObjLoader.Test
             const string title = "OpenGl test program";
             Console.WriteLine(title);
 
-            var improvedPerlinNoise = new ImprovedPerlinNoise();
+            var window = new OpenGlWindow(800, 600, title, OpenGlVersion.OpenGl31);
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (int i = 0; i < 10000000; i++)
-            {
-                var noise = improvedPerlinNoise.Noise(i, i, i);
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
-
-            stopwatch.Reset();
-
-            var simplexNoise = new SimplexNoise();
-            stopwatch.Start();
-
-            for (int i = 0; i < 10000000; i++)
-            {
-                var noise = simplexNoise.Noise(i, i);
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
-
-            //var window = new OpenGlWindow(800, 600, title, OpenGlVersion.OpenGl31);
-
-            //window.Run();
+            window.Run();
         }
     }
 }
--- source/CjClutter.OpenGl/OpenGlWindow.cs
+++ source/CjClutter.OpenGl/OpenGlWindow.cs
@@ -13,6 +13,9 @@ namespace CjClutter.OpenGl
 {
     public class OpenGlWindow : GameWindow
     {
+        private const int TerrainWidth = 256;
+        private const int TerrainHeight = 256;
+
         private readonly Dictionary<Key, Action<GameWindow>> _keyboardInputActions = new Dictionary<Key, Action<GameWindow>>
                                                                                          {
                                                                                              { Key.Escape, w => w.Exit() },
@@ -22,23 +25,29 @@ namespace CjClutter.OpenGl
         private readonly FrameTimeCounter _frameTimeCounter = new FrameTimeCounter();
         private SimplexNoise _simplexNoise;
         private Stopwatch _stopwatch;
+        private double[,] _heightMap;
 
         public OpenGlWindow(int width, int height, string title, OpenGlVersion openGlVersion)
             : base(
-            width, 
-            height, 
-            GraphicsMode.Default, 
-            title, 
-            GameWindowFlags.Default, 
-            DisplayDevice.Default, 
-            openGlVersion.Major, 
-            openGlVersion.Minor, 
-            GraphicsContextFlags.Default) {}
+            width,
+            height,
+            GraphicsMode.Default,
+            title,
+            GameWindowFlags.Default,
+            DisplayDevice.Default,
+            openGlVersion.Major,
+            openGlVersion.Minor,
+            GraphicsContextFlags.Default)
+        {
+            VSync = VSyncMode.Off;
+        }
 
         protected override void OnLoad(EventArgs e)
         {
             var font = new Font(FontFamily.GenericSansSerif, 10);
             _qFont = QFontFactory.Create(font);
+
+            _heightMap = new double[TerrainWidth, TerrainHeight];
 
             _simplexNoise = new SimplexNoise();
             _stopwatch = new Stopwatch();
@@ -51,19 +60,65 @@ namespace CjClutter.OpenGl
 
             _frameTimeCounter.UpdateFrameTime(e.Time);
 
+            var perspectiveMatrix = Matrix4d.CreatePerspectiveFieldOfView(Math.PI/4, 1, 1, 100);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiveMatrix);
+
+            var lookAtMatrix = Matrix4d.LookAt(2, 2, 0, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookAtMatrix);
+
             GL.ClearColor(Color4.White);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             GL.Color3(Color.Green);
 
-            GL.Begin(BeginMode.LineStrip);
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            GL.Begin(BeginMode.Quads);
 
+            var elapsedseconds = _stopwatch.Elapsed.TotalSeconds / 10;
 
-            for (int i = 0; i < 200; i++)
+            for (var i = 0; i < TerrainWidth; i++)
             {
-                var noise = _simplexNoise.Noise(i/10.0, _stopwatch.Elapsed.TotalSeconds/10);
-                GL.Vertex2(-1 + i / 100.0, noise);
+                for (var j = 0; j < TerrainHeight; j++)
+                {
+                    var xin = i/(double) TerrainWidth*2;
+                    var yin = j/(double) TerrainHeight*2;
+                    _heightMap[i, j] = 0.2 * _simplexNoise.Noise(xin, yin, elapsedseconds);
+                }
             }
+
+            for (var i = 0; i < TerrainWidth - 1; i++)
+            {
+                for (var j = 0; j < TerrainHeight - 1; j++)
+                {
+                    var x0 = -0.5 + ScaleTo(i, TerrainWidth);
+                    var x1 = -0.5 + ScaleTo(i + 1, TerrainWidth);
+                    var x2 = -0.5 + ScaleTo(i + 1, TerrainWidth);
+                    var x3 = -0.5 + ScaleTo(i, TerrainWidth);
+
+                    var y0 = -0.5 + ScaleTo(j, TerrainHeight);
+                    var y1 = -0.5 + ScaleTo(j, TerrainHeight);
+                    var y2 = -0.5 + ScaleTo(j + 1, TerrainHeight);
+                    var y3 = -0.5 + ScaleTo(j + 1, TerrainHeight);
+
+                    var z0 = _heightMap[i, j];
+                    var z1 = _heightMap[i + 1, j];
+                    var z2 = _heightMap[i + 1, j + 1];
+                    var z3 = _heightMap[i, j + 1];
+
+                    GL.Vertex3(x0, z0, y0);
+                    GL.Vertex3(x1, z1, y1);
+                    GL.Vertex3(x2, z2, y2);
+                    GL.Vertex3(x3, z3, y3);
+                }
+            }
+
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    var noise = _simplexNoise.Noise(i / 10.0, _stopwatch.Elapsed.TotalSeconds / 10);
+            //    GL.Vertex2(-1 + i / 100.0, noise);
+            //}
 
 
             GL.End();
@@ -78,9 +133,16 @@ namespace CjClutter.OpenGl
 
             //GL.End();
 
+            GL.PolygonMode(MaterialFace.Front, PolygonMode.Fill);
+
             DrawDebugText();
 
             SwapBuffers();
+        }
+
+        private double ScaleTo(double value, double max)
+        {
+            return value / max;
         }
 
         private void DrawDebugText()
