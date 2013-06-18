185d40098889721c99aa663347b232eb2b528c2a
Chris Jansson <chris.janssom@gmail.com>
2013-06-18 20:58:30 +0200

    A shoddy first try at input for parameter generation

--- source/CjClutter.OpenGl/SceneGraph/Hud.cs
+++ source/CjClutter.OpenGl/SceneGraph/Hud.cs
@@ -1,11 +1,68 @@
-﻿using Gwen;
+﻿using System;
+using System.ComponentModel;
+using CjClutter.OpenGl.Noise;
+using Gwen;
 using Gwen.Control;
 using Gwen.Skin;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using Base = Gwen.Control.Base;
 
 namespace CjClutter.OpenGl.SceneGraph
 {
+    public class GeneratoionSettingsControl
+    {
+        private readonly Base _parent;
+
+        private int _octaves;
+        private double _amplitude;
+        private double _frequency;
+        private TextBox _octavesTextBox;
+        private TextBox _amplitudeTextBox;
+        private TextBox _frequenceyTextBox;
+
+
+        public GeneratoionSettingsControl(Base parent)
+        {
+            _parent = parent;
+            _octavesTextBox = CreateTextBoxForPropety(() => _octaves, x => _octaves = x);
+            _amplitudeTextBox = CreateTextBoxForPropety(() => _amplitude, x => _amplitude = x);
+            _frequenceyTextBox = CreateTextBoxForPropety(() => _frequency, x => _frequency = x);
+        }
+
+        private TextBox CreateTextBoxForPropety<T>(Func<T> getter, Action<T> setter)
+        {
+            var textBox = new TextBox(_parent)
+                {
+                    Text = getter().ToString(),
+                    Dock = Pos.Top,
+                };
+
+            textBox.TextChanged += x =>
+                {
+                    var text = textBox.Text;
+                    var converter = TypeDescriptor.GetConverter(typeof (T));
+
+                    try
+                    {
+                        var newValue = (T)converter.ConvertFromInvariantString(text);
+                        setter(newValue);
+                    }
+                    catch (Exception e)
+                    {
+                        
+                    }
+                    
+                };
+            return textBox;
+        }
+
+        public FractalBrownianMotionSettings GetSettings()
+        {
+            return new FractalBrownianMotionSettings(_octaves, _amplitude, _frequency);
+        }
+    }
+
     public class Hud
     {
         private readonly Label _frameTimeLabel;
@@ -21,8 +78,8 @@ namespace CjClutter.OpenGl.SceneGraph
             _texturedBase = new TexturedBase(_renderer, "DefaultSkin.png");
             _canvas = new Canvas(_texturedBase);
 
-            //var input = new Gwen.Input.OpenTK(gameWindow);
-            //input.Initialize(_canvas);
+            var input = new Gwen.Input.OpenTK(gameWindow);
+            input.Initialize(_canvas);
 
             //_canvas.ShouldDrawBackground = true;
             //_canvas.BackgroundColor = Color.FromArgb(255, 150, 170, 170);
@@ -38,6 +95,17 @@ namespace CjClutter.OpenGl.SceneGraph
                     AutoSizeToContents = true, 
                     Dock = Pos.Top
                 };
+
+            var generatoionSettingsControl = new GeneratoionSettingsControl(_canvas);
+
+            gameWindow.Mouse.Move += (sender, args) => input.ProcessMouseMessage(args);
+            gameWindow.Mouse.ButtonDown += (sender, args) => input.ProcessMouseMessage(args);
+            gameWindow.Mouse.ButtonUp += (sender, args) => input.ProcessMouseMessage(args);
+            gameWindow.Mouse.WheelChanged += (sender, args) => input.ProcessMouseMessage(args);
+
+            gameWindow.Keyboard.KeyDown += (sender, args) => input.ProcessKeyDown(args);
+            gameWindow.Keyboard.KeyUp += (sender, args) => input.ProcessKeyUp(args);
+            gameWindow.KeyPress += (sender, args) => input.KeyPress(sender, args);
         }
 
         public void Resize(int width, int height)
