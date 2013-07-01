408faaade3b4ba3e05db17452ce6784709c678e7
Chris Jansson <chris.janssom@gmail.com>
2013-07-01 20:02:44 +0200

    Created a mesh type that contains vertices with normals and can calculate normals for faces

--- source/CjClutter.OpenGl/SceneGraph/Mesh.cs
+++ source/CjClutter.OpenGl/SceneGraph/Mesh.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CjClutter.OpenGl.OpenGl.VertexTypes;
 using OpenTK;
+using System.Linq;
 
 namespace CjClutter.OpenGl.SceneGraph
 {
@@ -17,5 +18,56 @@ namespace CjClutter.OpenGl.SceneGraph
         public Vector4 Color { get; set; }
 
         public Matrix4 ModelMatrix { get; set; }
+    }
+
+    public class Mesh3V3N
+    {
+        private readonly Vertex3V3N[] _vertices;
+        private readonly Face3[] _faces;
+
+        public Mesh3V3N(IEnumerable<Vertex3V3N> vertices, IEnumerable<Face3> faces)
+        {
+            _vertices = vertices.ToArray();
+            _faces = faces.ToArray();
+        }
+
+        public Vertex3V3N[] Vertices
+        {
+            get { return _vertices; }
+        }
+
+        public Face3[] Faces
+        {
+            get { return _faces; }
+        }
+
+        public void CalculateNormals()
+        {
+            for (var i = 0; i < _faces.Length; i++)
+            {
+                var face = _faces[i];
+
+                var v0 = _vertices[face.V0].Position;
+                var v1 = _vertices[face.V1].Position;
+                var v2 = _vertices[face.V2].Position;
+
+                var u = v1 - v0;
+                var v = v2 - v0;
+                u.Normalize();
+                v.Normalize();
+
+                var normal = Vector3.Cross(u, v);
+                normal.Normalize();
+
+                _vertices[face.V0].Normal += normal;
+                _vertices[face.V1].Normal += normal;
+                _vertices[face.V2].Normal += normal;
+            }
+
+            for (var index = 0; index < _vertices.Length; index++)
+            {
+                _vertices[index].Normal.Normalize();
+            }
+        }
     }
 }
