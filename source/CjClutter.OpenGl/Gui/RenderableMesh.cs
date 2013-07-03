ec1307437878d0752eed7f27385ddfc21eda4190
Chris Jansson <chris.janssom@gmail.com>
2013-07-03 17:57:59 +0200

    Allocate resoruces for mesh 3v3n

---
+++ source/CjClutter.OpenGl/Gui/RenderableMesh.cs
@@ -0,0 +1,31 @@
+﻿using CjClutter.OpenGl.OpenGl;
+using CjClutter.OpenGl.OpenGl.VertexTypes;
+
+namespace CjClutter.OpenGl.Gui
+{
+    public class RenderableMesh
+    {
+        private readonly VertexBufferObject<Vertex3V3N> _vertexBuffer;
+        private readonly VertexBufferObject<uint> _elementBuffer;
+        private readonly VertexArrayObject _vertexArrayObject;
+
+        public RenderableMesh(VertexBufferObject<Vertex3V3N> vertexBuffer, VertexBufferObject<uint> elementBuffer, VertexArrayObject vertexArrayObject)
+        {
+            _vertexArrayObject = vertexArrayObject;
+            _elementBuffer = elementBuffer;
+            _vertexBuffer = vertexBuffer;
+        }
+
+        public VertexArrayObject VertexArrayObject
+        {
+            get { return _vertexArrayObject; }
+        }
+
+        public void Delete()
+        {
+            _elementBuffer.Delete();
+            _vertexBuffer.Delete();
+            _vertexArrayObject.Delete();
+        }
+    }
+}
--- source/CjClutter.OpenGl/Gui/ResourceAllocator.cs
+++ source/CjClutter.OpenGl/Gui/ResourceAllocator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CjClutter.Commons.Reflection;
 using CjClutter.OpenGl.OpenGl;
 using CjClutter.OpenGl.OpenGl.VertexTypes;
 using CjClutter.OpenGl.SceneGraph;
@@ -8,12 +7,12 @@ using System.Linq;
 
 namespace CjClutter.OpenGl.Gui
 {
-    public class RenderableMesh
+    public interface IResourceAllocator
     {
-        
+        RenderableMesh AllocateResourceFor(Mesh3V3N mesh);
     }
 
-    public class ResourceAllocator
+    public class ResourceAllocator : IResourceAllocator
     {
         private readonly IOpenGlResourceFactory _resourceFactory;
 
@@ -22,15 +21,22 @@ namespace CjClutter.OpenGl.Gui
             _resourceFactory = resourceFactory;
         }
 
-        public void AllocateResourceFor(Mesh3V3N mesh)
+        public RenderableMesh AllocateResourceFor(Mesh3V3N mesh)
         {
-            var vertexBuffer = FillVertexBuffer(mesh);
-            var elementBuffer = FillElementBuffer(mesh);
+            var vertexBuffer = CreateVertexBuffer(mesh);
+            var elementBuffer = CreateElementBuffer(mesh);
+            var vertexArrayObject = CreateAndSetupVertexArrayObject(vertexBuffer, elementBuffer);
 
+            return new RenderableMesh(vertexBuffer, elementBuffer, vertexArrayObject);
+        }
+
+        private VertexArrayObject CreateAndSetupVertexArrayObject(VertexBufferObject<Vertex3V3N> vertexBuffer, VertexBufferObject<uint> elementBuffer)
+        {
             var vertexArrayObject = _resourceFactory.CreateVertexArrayObject();
 
             vertexArrayObject.Bind();
             vertexBuffer.Bind();
+            elementBuffer.Bind();
 
             var dummy = new Vertex3V3N();
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, dummy.SizeInBytes, dummy.PositionOffset);
@@ -39,17 +45,20 @@ namespace CjClutter.OpenGl.Gui
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, dummy.SizeInBytes, dummy.NormalOffset);
             GL.EnableVertexAttribArray(1);
 
-            vertexBuffer.Unbind();
             vertexArrayObject.Unbind();
+            vertexBuffer.Unbind();
+            elementBuffer.Unbind();
+
+            return vertexArrayObject;
         }
 
-        private VertexBufferObject<uint> FillElementBuffer(Mesh3V3N mesh)
+        private VertexBufferObject<uint> CreateElementBuffer(Mesh3V3N mesh)
         {
             //Todo pick index type depending on mesh size
             if (mesh.Vertices.LongLength > int.MaxValue) throw new NotImplementedException();
 
-            var indices = mesh.Faces.SelectMany(x => new[] {(uint) x.V0, (uint) x.V1, (uint) x.V2}).ToArray();
-            var elementBuffer = _resourceFactory.CreateVertexBufferObject<uint>(BufferTarget.ElementArrayBuffer, sizeof (uint));
+            var indices = mesh.Faces.SelectMany(x => new[] { (uint)x.V0, (uint)x.V1, (uint)x.V2 }).ToArray();
+            var elementBuffer = _resourceFactory.CreateVertexBufferObject<uint>(BufferTarget.ElementArrayBuffer, sizeof(uint));
             elementBuffer.Bind();
 
             elementBuffer.Data(indices);
@@ -58,12 +67,12 @@ namespace CjClutter.OpenGl.Gui
             return elementBuffer;
         }
 
-        private VertexBufferObject<Vertex3V3N> FillVertexBuffer(Mesh3V3N mesh)
+        private VertexBufferObject<Vertex3V3N> CreateVertexBuffer(Mesh3V3N mesh)
         {
             var vertexBuffer = _resourceFactory.CreateVertexBufferObject<Vertex3V3N>(BufferTarget.ArrayBuffer);
 
             vertexBuffer.Bind();
-            vertexBuffer.Data(mesh.Vertices, BufferUsageHint.StaticDraw);
+            vertexBuffer.Data(mesh.Vertices);
             vertexBuffer.Unbind();
 
             return vertexBuffer;
