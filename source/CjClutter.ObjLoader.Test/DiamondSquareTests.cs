a77ed0f21ae553a1b53e5ddb11d3e40bc7f58298
Chris Jansson <chris.janssom@gmail.com>
2014-06-13 21:01:52 +0200

    Start of diamond square

---
+++ source/CjClutter.ObjLoader.Test/DiamondSquareTests.cs
@@ -0,0 +1,33 @@
+﻿using CjClutter.OpenGl.Noise;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ObjLoader.Test
+{
+    public class DiamondSquareTests
+    {
+        private DiamondSquare _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new DiamondSquare();
+        }
+
+        [Test]
+        public void Zero_sub_divisions_returns_original_corners()
+        {
+            var result = _sut.Generate(0, 2, 4, 8, 0);
+
+            result.Should().BeEquivalentTo(0.0, 2.0, 4.0, 8.0);
+        }
+
+        [Test]
+        public void One_sub_division_returns_sub_divided_rectangles()
+        {
+            var result = _sut.Generate(0, 2, 4, 8, 1);
+
+            result.Should().BeEquivalentTo(0.0, 1.0, 2.0, 2.0, 3.5, 5.0, 4.0, 6.0, 8.0);
+        }
+    }
+}
---
+++ source/CjClutter.OpenGl/Noise/DiamondSquare.cs
@@ -0,0 +1,25 @@
+﻿namespace CjClutter.OpenGl.Noise
+{
+    public class DiamondSquare
+    {
+        public double[] Generate(double h0, double h1, double h2, double h3, int levels)
+        {
+            var old = new[] { h0, h1, h2, h3 };
+            var result = old;
+
+            for (var i = 0; i < levels; i++)
+            {
+                Subdivide(old, i);
+
+
+            }
+
+            return result;
+        }
+
+        private void Subdivide(double[] old, int level)
+        {
+
+        }
+    }
+}
