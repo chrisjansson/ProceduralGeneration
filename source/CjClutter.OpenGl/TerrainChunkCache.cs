6c41933bda6a33dbd081ab84dca4a8153eaf48f4
Chris Jansson <chris.janssom@gmail.com>
2014-08-28 16:52:25 +0200

    Load chunks with tasks on the threadpool

--- source/CjClutter.OpenGl/TerrainChunkCache.cs
+++ source/CjClutter.OpenGl/TerrainChunkCache.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using CjClutter.OpenGl.EntityComponent;
 using CjClutter.OpenGl.Gui;
 using CjClutter.OpenGl.SceneGraph;
@@ -11,59 +10,15 @@ namespace CjClutter.OpenGl
     public class TerrainChunkCache
     {
         private readonly ConcurrentDictionary<Box3D, RenderableMesh> _cache = new ConcurrentDictionary<Box3D, RenderableMesh>();
-        private TerrainChunkFactory _terrainChunkFactory;
-        private List<Box3D> _queue;
-        private IResourceAllocator _resourceAllocator;
-        private Dictionary<Box3D, Mesh3V3N> _mapping;
+        private readonly TerrainChunkFactory _terrainChunkFactory;
+        private readonly IResourceAllocator _resourceAllocator;
+        private readonly Dictionary<Box3D, Task<Mesh3V3N>> _jobs;
 
         public TerrainChunkCache(TerrainChunkFactory terrainChunkFactory, IResourceAllocator resourceAllocator)
         {
             _resourceAllocator = resourceAllocator;
             _terrainChunkFactory = terrainChunkFactory;
-            _queue = new List<Box3D>();
-            _mapping = new Dictionary<Box3D, Mesh3V3N>();
-
-            for (int i = 0; i < 1; i++)
-            {
-                var workerThread = new Thread(() => Worker());
-                workerThread.IsBackground = true;
-                workerThread.Start();
-            }
-        }
-
-        private void Worker()
-        {
-            while (true)
-            {
-                Box3D bounds;
-                lock (this)
-                {
-                    if (!_queue.Any())
-                    {
-                        continue;
-                    }
-
-                    bounds = _queue[0];
-                }
-
-                var mesh = _terrainChunkFactory.Create(bounds);
-                lock (this)
-                {
-                    _queue.Remove(bounds);
-                    _mapping[bounds] = mesh;
-                }
-            }
-        }
-
-        public void LoadIfNotCachedAsync(Box3D bounds)
-        {
-            lock (this)
-            {
-                if (!_cache.ContainsKey(bounds) && !_queue.Contains(bounds))
-                {
-                    _queue.Insert(0, bounds);
-                }
-            }
+            _jobs = new Dictionary<Box3D, Task<Mesh3V3N>>();
         }
 
         public RenderableMesh GetRenderable(Box3D bounds)
@@ -75,20 +30,25 @@ namespace CjClutter.OpenGl
                 return mesh;
             }
 
-
-            lock (this)
+            if (!_jobs.ContainsKey(bounds))
             {
-                if (!_mapping.ContainsKey(bounds))
-                {
-                    return null;
-                }
-                var mesh2 = _mapping[bounds];
-                var renderableMesh = _resourceAllocator.AllocateResourceFor(mesh2);
-                _cache[bounds] = renderableMesh;
-                _mapping.Remove(bounds);
+                var inProgressTask = Task.Run(() => _terrainChunkFactory.Create(bounds));
+                _jobs.Add(bounds, inProgressTask);
+                return null;
             }
 
-            return _cache[bounds];
+            var meshTask = _jobs[bounds];
+            if (!meshTask.IsCompleted)
+            {
+                return null;
+            }
+
+            _jobs.Remove(bounds);
+            var allocatedMesh = meshTask.Result;
+            mesh = _resourceAllocator.AllocateResourceFor(allocatedMesh);
+            _cache[bounds] = mesh;
+
+            return mesh;
         }
     }
 }
