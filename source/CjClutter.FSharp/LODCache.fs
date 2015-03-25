3827625af049b1d337c16af0ef6824c2e77573c3
Chris Jansson <chris.janssom@gmail.com>
2015-03-25 17:45:54 +0100

    Begin caching lod cache

---
+++ source/CjClutter.FSharp/LODCache.fs
@@ -0,0 +1,15 @@
+﻿module LODCache
+open CjClutter.OpenGl
+
+type node = ChunkedLodTreeFactory.ChunkedLodTreeNode
+
+type LODCache = {
+        contains : node -> bool
+        beginCache : node -> unit
+        get : node -> primitives.meshWithNormals
+    }
+
+let makeLODCache = 
+    
+
+
