eb56ede2a0663309b0b8473b3ba033b0c108904f
Chris Jansson <chris.janssom@gmail.com>
2015-03-26 20:27:22 +0100

    Continued

--- source/CjClutter.FSharp/LODCache.fs
+++ source/CjClutter.FSharp/LODCache.fs
@@ -9,7 +9,20 @@ type LODCache = {
         get : node -> primitives.meshWithNormals
     }
 
-let makeLODCache = 
-    
+let getNodesToDraw cache (requestedNodes:node array) =
+    let rec getNodesToDrawInternal (requested, notCached) =
+        let allAreCached = requested |> Array.forall (fun n -> cache.contains n)
+        let isRoot = requestedNodes.Length = 1
+        match allAreCached || isRoot with
+        | true -> (requestedNodes, notCached)
+        | _ -> 
+            let notCachedNodes = requestedNodes |> Array.filter (fun n -> not (cache.contains n))
+            let notCachedNodesParents = notCachedNodes |> Array.map (fun n -> n.Parent) |> Array.distinct
+            let cachedNodes = requestedNodes |> Array.filter (fun n -> cache.contains n)
+            let cachedNodesReadyToBeDrawn = cachedNodes |> Array.filter (fun n -> not (Array.contains n notCachedNodesParents))
+            let nodesToRequest = Array.concat [cachedNodesReadyToBeDrawn; notCachedNodesParents]
+            getNodesToDrawInternal (nodesToRequest, Array.concat [notCachedNodes; notCached])
+
+    getNodesToDrawInternal (requestedNodes, [||])
 
 
