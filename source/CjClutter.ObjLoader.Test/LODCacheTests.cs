a8f484aaf656894393dce870e5d24ddda624b359
Chris Jansson <chris.janssom@gmail.com>
2015-04-09 19:25:22 +0200

    Pulled out some common code to make tests cleaner

--- source/CjClutter.ObjLoader.Test/LODCacheTests.cs
+++ source/CjClutter.ObjLoader.Test/LODCacheTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CjClutter.OpenGl;
 using CjClutter.OpenGl.EntityComponent;
 using NUnit.Framework;
@@ -7,19 +8,18 @@ namespace ObjLoader.Test
 {
     public class LodCacheTests
     {
+        private List<ChunkedLodTreeFactory.ChunkedLodTreeNode> _cache;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cache = new List<ChunkedLodTreeFactory.ChunkedLodTreeNode>();
+        }
+
         [Test]
         public void No_nodes_are_requested_or_to_cache_when_none_are_visible()
         {
-            Func<ChunkedLodTreeFactory.ChunkedLodTreeNode, bool> contains = _ => false;
-            Action<ChunkedLodTreeFactory.ChunkedLodTreeNode> beginCache = _ => { };
-            Func<ChunkedLodTreeFactory.ChunkedLodTreeNode, Rendering.AllocatedMesh> get = _ => { throw new Exception(); };
-            var a = FSharpUtil.FSharpFuncUtil.ToFSharpFunc(contains);
-            var b = FSharpUtil.FSharpFuncUtil.ToFSharpFunc(beginCache);
-            var c = FSharpUtil.FSharpFuncUtil.ToFSharpFunc(get);
-            var cache = new LODCache.LODCache(
-                a,
-                b,
-                c);
+            var cache = GetCache();
 
             var result = LODCache.getNodesToDrawAndCache(cache, new ChunkedLodTreeFactory.ChunkedLodTreeNode[] { });
 
@@ -28,9 +28,24 @@ namespace ObjLoader.Test
         }
 
         [Test]
-        public void All_nodse_are_requested_when_all_are_cached()
+        public void All_nodese_are_requested_when_all_are_cached()
         {
-            Func<ChunkedLodTreeFactory.ChunkedLodTreeNode, bool> contains = _ => true;
+            var node0 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), null, 0);
+            var node1 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), node0, 0);
+            var node2 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), node1, 0);
+            var node3 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), null, 0);
+            CacheNodes(node0, node1, node2, node3);
+
+            var cache = GetCache();
+            var result = LODCache.getNodesToDrawAndCache(cache, new[] { node0, node1, node2, node3 });
+
+            CollectionAssert.AreEqual(new[] { node0, node1, node2, node3 }, result.Item1);
+            Assert.IsEmpty(result.Item2);
+        }
+
+        private LODCache.LODCache GetCache()
+        {
+            Func<ChunkedLodTreeFactory.ChunkedLodTreeNode, bool> contains = node => _cache.Contains(node);
             Action<ChunkedLodTreeFactory.ChunkedLodTreeNode> beginCache = _ => { };
             Func<ChunkedLodTreeFactory.ChunkedLodTreeNode, Rendering.AllocatedMesh> get = _ => { throw new Exception(); };
             var a = FSharpUtil.FSharpFuncUtil.ToFSharpFunc(contains);
@@ -41,14 +56,20 @@ namespace ObjLoader.Test
                 b,
                 c);
 
-            var node0 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), null, 0);
-            var node1 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), node0, 0);
-            var node2 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), node1, 0);
-            var node3 = new ChunkedLodTreeFactory.ChunkedLodTreeNode(new Box3D(), null, 0);
-            var result = LODCache.getNodesToDrawAndCache(cache, new[] { node0, node1, node2, node3 });
+            return cache;
+        }
 
-            CollectionAssert.AreEqual(new[] { node0, node1, node2, node3 }, result.Item1);
-            Assert.IsEmpty(result.Item2);
+        private void CacheNodes(params ChunkedLodTreeFactory.ChunkedLodTreeNode[] nodes)
+        {
+            foreach (var chunkedLodTreeNode in nodes)
+            {
+                CacheNode(chunkedLodTreeNode);
+            }
+        }
+
+        private void CacheNode(ChunkedLodTreeFactory.ChunkedLodTreeNode node)
+        {
+            _cache.Add(node);
         }
     }
 }
