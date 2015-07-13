23a8153c4164094387e87532aef133001dad7e88
Chris Jansson <chris.janssom@gmail.com>
2015-07-13 22:42:09 +0200

    First lod selection tests

---
+++ source/CDLod.Tests/LodSelectTests.fs
@@ -0,0 +1,17 @@
+﻿module LodSelectTests
+
+open Xunit
+open LodSelect
+open Swensen.Unquote
+
+[<Fact>]
+let ``lod selection where root node is out of frustum should be empty`` () =
+    let node = { Children = [] }
+    let actual = lodSelect (fun _ -> false) node
+    test <@ actual = list.Empty @>
+
+[<Fact>]
+let ``lod selection where root node is inside the frustum and of sufficient detail stops at root node`` () =
+    let node = { Children = [] }
+    let actual = lodSelect (fun _ -> true) node
+    test <@ actual = [ node]  @>
---
+++ source/CDLod/LodSelect.fs
@@ -0,0 +1,12 @@
+﻿module LodSelect
+
+type Node = {
+        Children : Node list
+    }
+
+let lodSelect frustumTester root =
+    //test against frustum
+    //sphere intersect
+    match frustumTester root with
+    | true -> [ root ]
+    | false -> []
