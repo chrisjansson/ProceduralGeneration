ac51fafef2c4ad2abfcee9ad73b62e0721c2439f
Chris Jansson <chris.janssom@gmail.com>
2015-07-14 21:10:53 +0200

    Added a memory comment for future performance improvement
    Did a bit of reading on the algo as well

--- source/CDLod/LodSelect.fs
+++ source/CDLod/LodSelect.fs
@@ -4,6 +4,8 @@ type Node = {
         Children : Node list
     }
 
+//Optimization, if a parent node is completely inside the frustum all children will be that as well -> frustum cull can be skipped
+
 let lodSelect frustumTester root =
     //test against frustum
     //sphere intersect
