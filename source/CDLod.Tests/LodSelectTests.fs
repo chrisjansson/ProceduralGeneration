03200507063844288a945f5258fca30365223c3c
Chris Jansson <chris.janssom@gmail.com>
2015-07-20 17:56:21 +0200

    Wip

--- source/CDLod.Tests/LodSelectTests.fs
+++ source/CDLod.Tests/LodSelectTests.fs
@@ -4,31 +4,66 @@ open Xunit
 open LodSelect
 open Swensen.Unquote
 
+
 [<Fact>]
 let ``lod selection where root node is out of frustum should be empty`` () =
     let node = { Children = []; LodLevel = 0 }
-    let actual = lodSelect (fun _ -> false) (fun _ -> true) node
+    let actual = lodSelect (fun _ -> false) (fun _ _ -> true) node
     test <@ actual = list.Empty @>
 
 [<Fact>]
-let ``lod selection where node is inside the frustum in range and the last level selects node`` () =
+let ``lod selection where root node is in frustum and has no children selects root`` () =
     let node = { Children = []; LodLevel = 0 }
-    let actual = lodSelect (fun _ -> true) (fun _ -> true) node
-    test <@ actual = [ node]  @>
-
-[<Fact>]
-let ``lod selection where root node is inside the frustum and out of rangeshould be empty`` () =
-    let node = { Children = []; LodLevel = 0 }
-    let actual = lodSelect (fun _ -> true) (fun _ -> false) node
-    test <@ actual = list.Empty  @>
-
-[<Fact>]
-let ``lod selection where node is inside the frustum in range and not in range of next lod level selects node`` () =
-    let node = {
-                Children = [ { Children = []; LodLevel = -1; } ]
-                LodLevel = 1
-                }
-
-    let detailSelector = (fun level -> match level with 1 -> true | _ -> false)
-    let actual = lodSelect (fun _ -> true) detailSelector node
+    let actual = lodSelect (fun _ -> true) (fun _ _ -> true) node
     test <@ actual = [ node ] @>
+
+[<Fact>]
+let ``lod selection where root node is in range of the next lod level and child node is in range of it's lod level selects child`` () =
+    let rootLod = 1
+    let childLod = 10
+    let child = { Children = []; LodLevel = childLod };
+    let node = { Children = [ child ]; LodLevel = rootLod; }
+    let actual = lodSelect (fun _ -> true) (fun _ _ -> true) node
+    test <@ actual = [ child ] @>
+
+
+[<Fact>]
+let ``lod selection where root node is not in range of the next lod level selects root`` () =
+    let node = { Children = [ { Children = []; LodLevel = 1 } ]; LodLevel = 0 }
+    let actual = lodSelect (fun _ -> true) (fun _ _ -> false) node
+    test <@ actual = [ node ] @>
+
+//
+//[<Fact>]
+//let ``lod selection where node is inside the frustum in range and the last level selects node`` () =
+//    let node = { Children = []; LodLevel = 0 }
+//    let actual = lodSelect (fun _ -> true) (fun _ -> true) node
+//    test <@ actual = [ node]  @>
+//
+//[<Fact>]
+//let ``lod selection where root node is inside the frustum and out of rangeshould be empty`` () =
+//    let node = { Children = []; LodLevel = 0 }
+//    let actual = lodSelect (fun _ -> true) (fun _ -> false) node
+//    test <@ actual = list.Empty  @>
+//
+//[<Fact>]
+//let ``lod selection where node is inside the frustum in range and not in range of next lod level selects node`` () =
+//    let node = {
+//                Children = [ { Children = []; LodLevel = -1; } ]
+//                LodLevel = 1
+//            }
+//
+//    let detailSelector = (fun level -> match level with 1 -> true | _ -> false)
+//    let actual = lodSelect (fun _ -> true) detailSelector node
+//    test <@ actual = [ node ] @>
+//
+//[<Fact>]
+//let ``lod selection where node is inside the frustum in range and in range of next lod level selects child node`` () =
+//    let node = {
+//                Children = [ { Children = []; LodLevel = 2; } ]
+//                LodLevel = 1
+//            }
+//
+//    let detailSelector = (fun level -> match level with 1 | 2 -> true | _ -> false)
+//    let actual = lodSelect (fun _ -> true) detailSelector node
+//    test <@ actual = [ { Children = []; LodLevel = 2; } ] @>
--- source/CDLod/LodSelect.fs
+++ source/CDLod/LodSelect.fs
@@ -18,11 +18,20 @@ type Node = {
 
 //Optimization, if a parent node is completely inside the frustum all children will be that as well -> frustum cull can be skipped
 
-let lodSelect frustumTester detailTester node =
+let lodSelect frustumTester detailTester node =      
     let intersectsFrustum = frustumTester node
-    let isInRange = detailTester node.LodLevel
-    //test against frustum
-    //sphere intersect
-    match (intersectsFrustum, isInRange) with
-    | (true, true) -> [ node ]
+    let hasChildren = not node.Children.IsEmpty
+    match (intersectsFrustum, hasChildren) with
+    | (true, false) -> [ node ]
+    | (true, true) -> 
+        let isInNextLodRange = detailTester node (node.LodLevel + 1)
+        match isInNextLodRange with
+        | true -> node.Children
+        | false -> [ node ]
     | _ -> []
+//    let isInRange = detailTester node.LodLevel
+//    //test against frustum
+//    //sphere intersect
+//    match (intersectsFrustum, isInRange) with
+//    | (true, true) -> [ node ]
+//    | _ -> []
