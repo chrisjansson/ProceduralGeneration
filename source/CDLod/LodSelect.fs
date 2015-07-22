5803f37c690786f7aef0b49f53427b54713cb50f
Chris Jansson <chris.janssom@gmail.com>
2015-07-22 22:13:43 +0200

    Refactored LodSelect

--- source/CDLod/LodSelect.fs
+++ source/CDLod/LodSelect.fs
@@ -26,19 +26,18 @@ type Selection =
     | Partial of PartialSelection
 
 //Optimization, if a parent node is completely inside the frustum all children will be that as well -> frustum cull can be skipped
-
 let rec lodSelect frustumTester detailTester node =      
     let intersectsFrustum = frustumTester node
-    let hasChildren = not node.Children.IsEmpty
-    match (intersectsFrustum, hasChildren) with
-    | (true, false) -> [ Full node ]
-    | (true, true) -> 
+    match intersectsFrustum with
+    | true ->
+        let hasChildren = not node.Children.IsEmpty
         let isInNextLodRange = detailTester node (node.LodLevel + 1)
         let converter n =
             match detailTester n n.LodLevel with
             | true -> lodSelect frustumTester detailTester n
             | false -> [ Partial { Parent = node; Child = n } ]
-        match isInNextLodRange with
-        | true -> List.collect converter node.Children
-        | false -> [ Full node ]
-    | _ -> []
+        match (hasChildren, isInNextLodRange) with
+        | (false, _) -> [ Full node ]
+        | (true, false) -> [ Full node]
+        | (true, true) -> List.collect converter node.Children
+    | false -> []
