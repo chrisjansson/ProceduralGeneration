cef242e0b542751d3d8c3c05b54bd30022712835
Chris Jansson <chris.janssom@gmail.com>
2015-07-23 17:08:30 +0200

    Tree traversal and lod splitting

--- source/CDLod.Tests/QuadTreeTests.fs
+++ source/CDLod.Tests/QuadTreeTests.fs
@@ -13,6 +13,39 @@ let zeroBounds = {
     }
 
 [<Fact>]
-let ``QuadTree with depth of zero contains specified bounds and has no children`` () =
-    let actual = makeQuadTree 0 zeroBounds
+let ``Tree with depth of zero contains specified bounds and has no children`` () =
+    let actual = makeTree (fun _ -> []) 0 zeroBounds
     test <@ { Children = []; LodLevel = 0; Bounds = zeroBounds } = actual @>
+
+[<Fact>]
+let ``Tree with depth of one contains specified bounds and has one child because of singular bounds split`` () =
+    let actual = makeTree (fun _ -> [ zeroBounds ]) 1 zeroBounds
+    test <@ { Children = [ { Children = []; LodLevel = 0; Bounds = zeroBounds } ]; LodLevel = 1; Bounds = zeroBounds } = actual @>
+
+[<Fact>]
+let ``Tree with depth of one contains specified bounds and has three children because of bounds split`` () =
+    let actual = makeTree (fun _ -> [ zeroBounds; zeroBounds; zeroBounds ]) 1 zeroBounds
+    test <@ { 
+                Children = [ 
+                                { Children = []; LodLevel = 0; Bounds = zeroBounds } 
+                                { Children = []; LodLevel = 0; Bounds = zeroBounds } 
+                                { Children = []; LodLevel = 0; Bounds = zeroBounds } 
+                            ]; 
+                LodLevel = 1; 
+                Bounds = zeroBounds 
+            } = actual @>
+
+[<Fact>]
+let ``Splits bounds in four pieces in XZ plane``() =
+    let bounds = { Min = { X = -1.0; Y = -10.0; Z = -1.0; }; Max = { X = 1.0; Y = 10.0; Z = 1.0; } }
+    let actual = quadXZBoundsSplitter bounds
+
+    let expected = 
+        [
+            { Min = { X = -1.0; Y = -10.0; Z = -1.0; }; Max = { X = 0.0; Y = 10.0; Z = 0.0; } }
+            { Min = { X = -1.0; Y = -10.0; Z = 0.0; }; Max = { X = 0.0; Y = 10.0; Z = 1.0; } }
+            { Min = { X = 0.0; Y = -10.0; Z = -1.0; }; Max = { X = 1.0; Y = 10.0; Z = 0.0; } }
+            { Min = { X = 0.0; Y = -10.0; Z = 0.0; }; Max = { X = 1.0; Y = 10.0; Z = 1.0; } }
+        ]
+
+    test <@ expected = actual @>
--- source/CDLod/QuadTree.fs
+++ source/CDLod/QuadTree.fs
@@ -2,5 +2,40 @@
 
 open LodSelect
 
-let makeQuadTree d bounds =
-    { Children = []; LodLevel = 0; Bounds = bounds }
+let rec makeTree (splitter:Bounds -> Bounds list) depth bounds =
+    match depth with
+    | 0 -> { Children = []; LodLevel = depth; Bounds = bounds }
+    | d -> 
+        { 
+            Children = bounds |> splitter |> List.map (fun b -> makeTree splitter (d - 1) b)
+            LodLevel = d
+            Bounds = bounds
+        }
+
+let quadXZBoundsSplitter bounds =
+    let max = bounds.Max
+    let min = bounds.Min
+    let center = {
+            X = min.X + (max.X - min.X) / 2.0
+            Y = 0.0
+            Z = min.Z + (max.Z - min.Z) / 2.0
+        } 
+
+    [
+        { 
+            Min = min; 
+            Max = { X = center.X; Y = max.Y; Z = center.Z }
+        }
+        { 
+            Min = { X = min.X; Y = min.Y; Z = center.Z } 
+            Max = { X = center.X; Y = max.Y; Z = max.Z } 
+        }
+        { 
+            Min = { X = center.X; Y = min.Y; Z = min.Z } 
+            Max = { X = max.X; Y = max.Y; Z = center.Z } 
+        }
+        { 
+            Min = { X = center.X; Y = min.Y; Z = center.Z }
+            Max = max
+        }
+    ]
