f4bfcb710533c38d7ee0f6418b8557e893955287
Chris Jansson <chris.janssom@gmail.com>
2015-07-26 21:58:12 +0200

    Added Arvo AABB sphere intersection

---
+++ source/CDLod.Tests/AABBSphereInteresectionTests.fs
@@ -0,0 +1,25 @@
+﻿module AABBSphereInteresectionTests
+
+open Xunit
+open Swensen.Unquote
+
+[<Fact>]
+let ``Sphere intersects with edge of cube`` () =
+    let aabb = { Min = { X = -1.0; Y = -1.0; Z = 0.0; }; Max = { X = 1.0; Y = 1.0; Z = 0.0 } }
+    let sphere = { Center = { X = 0.0; Y = 2.0; Z = 0.0; }; Radius = 1.0; }
+
+    test <@ aabbSphereIntersects aabb sphere = true @>
+
+[<Fact>]
+let ``Sphere does not intersect with cube`` () =
+    let aabb = { Min = { X = -1.0; Y = -1.0; Z = 0.0; }; Max = { X = 1.0; Y = 1.0; Z = 0.0 } }
+    let sphere = { Center = { X = 0.0; Y = 2.0; Z = 0.0; }; Radius = 1.1; }
+
+    test <@ aabbSphereIntersects aabb sphere = true @>
+
+[<Fact>]
+let ``Sphere contained by cube intersects cube`` () =
+    let aabb = { Min = { X = -1.0; Y = -1.0; Z = 0.0; }; Max = { X = 1.0; Y = 1.0; Z = 0.0 } }
+    let sphere = { Center = { X = 0.0; Y = 0.0; Z = 0.0; }; Radius = 0.0; }
+
+    test <@ aabbSphereIntersects aabb sphere = true @>
--- source/CDLod/Math.fs
+++ source/CDLod/Math.fs
@@ -11,3 +11,34 @@ type Bounds = {
         Min : Point
         Max : Point
     }
+
+type Sphere = {
+        Center : Point
+        Radius : float
+    }
+
+let aabbSphereIntersects aabb sphere =
+    let mutable d = 0.0
+    
+    if sphere.Center.X < aabb.Min.X then
+        let s = sphere.Center.X - aabb.Min.X
+        d <- d + s * s    
+    else if sphere.Center.X > aabb.Max.X then
+        let s = sphere.Center.X - aabb.Max.X
+        d <- d + s * s
+
+    if sphere.Center.Y < aabb.Min.Y then
+        let s = sphere.Center.Y - aabb.Min.Y
+        d <- d + s * s    
+    else if sphere.Center.Y > aabb.Max.Y then
+        let s = sphere.Center.Y - aabb.Max.Y
+        d <- d + s * s
+
+    if sphere.Center.Z < aabb.Min.Z then
+        let s = sphere.Center.Z - aabb.Min.Z
+        d <- d + s * s    
+    else if sphere.Center.Z > aabb.Max.Z then
+        let s = sphere.Center.Z - aabb.Max.Z
+        d <- d + s * s
+
+    d <= sphere.Radius * sphere.Radius
