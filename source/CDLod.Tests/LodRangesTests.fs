a4b66d1bbaf60bb44f049268b445402486456be1
Chris Jansson <chris.janssom@gmail.com>
2015-07-27 22:01:18 +0200

    Calculate total detail balance, used for lod range calculation

---
+++ source/CDLod.Tests/LodRangesTests.fs
@@ -0,0 +1,20 @@
+﻿module LodRangesTests
+
+open Xunit
+open Swensen.Unquote
+open LodRanges
+
+[<Fact>]
+let ``Calculates total detail balance for zero lod levels`` () =
+    let actual = calculateTotalDetailBalance 0 2.0
+    test <@ actual = 0.0 @>
+
+[<Fact>]
+let ``Calculates total detail balance for one lod level`` () =
+    let actual = calculateTotalDetailBalance 1 2.0
+    test <@ actual = 1.0 @>
+
+[<Fact>]
+let ``Calculates total detail balance for three lod levels`` () =
+    let actual = calculateTotalDetailBalance 3 2.0
+    test <@ actual = 7.0 @>
---
+++ source/CDLod/LodRanges.fs
@@ -0,0 +1,17 @@
+﻿module LodRanges
+
+type LodSettings = {
+        LodLevels : int
+        LodDistanceRatio : float
+    }
+
+let calculateTotalDetailBalance lodLevels lodDistanceRatio =
+    let mutable totalDetailBalance = 0.0
+    let mutable currentDetailBalance = 1.0
+    for i = 0 to lodLevels - 1 do
+        totalDetailBalance <- totalDetailBalance + currentDetailBalance
+        currentDetailBalance <- currentDetailBalance * lodDistanceRatio
+    totalDetailBalance
+
+let makeLodRanges lodSettings =
+    ()
