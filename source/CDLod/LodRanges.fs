d2425d5e3acce095bdcd2007f42dfb5089d354cf
Chris Jansson <chris.janssom@gmail.com>
2015-07-28 17:02:20 +0200

    Calculate lod ranges

--- source/CDLod/LodRanges.fs
+++ source/CDLod/LodRanges.fs
@@ -3,6 +3,7 @@
 type LodSettings = {
         LodLevels : int
         LodDistanceRatio : float
+        VisibilityDistance : float
     }
 
 let calculateTotalDetailBalance lodLevels lodDistanceRatio =
@@ -14,4 +15,17 @@ let calculateTotalDetailBalance lodLevels lodDistanceRatio =
     totalDetailBalance
 
 let makeLodRanges lodSettings =
-    ()
+    let lodNear = 0.0
+    let lodFar = lodSettings.VisibilityDistance
+    let totalDetailBalance = calculateTotalDetailBalance lodSettings.LodLevels lodSettings.LodDistanceRatio
+    let sect = (lodFar - lodNear) / totalDetailBalance
+
+    let mutable previousPosition = lodNear
+    let mutable currentDetailBalance = 1.0
+    let lodRanges = Array.zeroCreate<float> lodSettings.LodLevels
+    for i = 0 to lodSettings.LodLevels - 1 do
+        let visibilityRange = previousPosition + sect * currentDetailBalance
+        lodRanges.[i] <- visibilityRange
+        previousPosition <- visibilityRange
+        currentDetailBalance <- currentDetailBalance * lodSettings.LodDistanceRatio
+    lodRanges
