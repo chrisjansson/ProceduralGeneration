d4615ef156b477e1913549e705aa86aeb243dfe5
Chris Jansson <chris.janssom@gmail.com>
2015-07-29 17:18:29 +0200

    Calculate morph start and end

--- source/CDLod.Tests/LodRangesTests.fs
+++ source/CDLod.Tests/LodRangesTests.fs
@@ -18,3 +18,23 @@ let ``Calculates total detail balance for one lod level`` () =
 let ``Calculates total detail balance for three lod levels`` () =
     let actual = calculateTotalDetailBalance 3 2.0
     test <@ actual = 7.0 @>
+
+[<Fact>]
+let ``Calculates lod ranges`` () =
+    let firstExpected = { VisibilityRange = 571.42857148571; MorphStart = 400.0; MorphEnd = 571.42857148571 }
+    let secondExpected = { VisibilityRange = 1714.285714; MorphStart = 1320.0; MorphEnd = 1714.285714 }
+    let thirdExpected = { VisibilityRange = 4000.0; MorphStart = 3196.0; MorphEnd = 4000.0 }
+    
+    let actual = makeLodRanges { LodLevels = 3; LodDistanceRatio = 2.0; VisibilityDistance = 4000.0; MorphStartRatio = 0.7 }
+    let first = actual.[0]
+    let second = actual.[1]
+    let third = actual.[2]
+
+    let assertLodRange expected actual =
+        Assert.Equal(expected.VisibilityRange, actual.VisibilityRange, 5)
+        Assert.Equal(expected.MorphStart, actual.MorphStart, 5)
+        Assert.Equal(expected.MorphEnd, actual.MorphEnd, 5)
+
+    assertLodRange firstExpected first
+    assertLodRange secondExpected second
+    assertLodRange thirdExpected third
--- source/CDLod/LodRanges.fs
+++ source/CDLod/LodRanges.fs
@@ -2,8 +2,9 @@
 
 type LodSettings = {
         LodLevels : int
-        LodDistanceRatio : float
+        LodDistanceRatio : float //2.0
         VisibilityDistance : float
+        MorphStartRatio : float //
     }
 
 let calculateTotalDetailBalance lodLevels lodDistanceRatio =
@@ -28,10 +29,26 @@ let makeLodRanges lodSettings =
 
     let mutable previousPosition = lodNear
     let mutable currentDetailBalance = 1.0
-    let lodRanges = Array.zeroCreate<float> lodSettings.LodLevels
+    let visibilityRanges = Array.zeroCreate<float> lodSettings.LodLevels
     for i = 0 to lodSettings.LodLevels - 1 do
         let visibilityRange = previousPosition + sect * currentDetailBalance
-        lodRanges.[i] <- visibilityRange
+        visibilityRanges.[i] <- visibilityRange
         previousPosition <- visibilityRange
         currentDetailBalance <- currentDetailBalance * lodSettings.LodDistanceRatio
+
+    let lodRanges = Array.zeroCreate<LodRange> lodSettings.LodLevels
+    previousPosition <- lodNear
+    for i = 0 to lodSettings.LodLevels - 1 do
+        let lodRange = {
+                VisibilityRange = visibilityRanges.[i]
+                MorphEnd = visibilityRanges.[i]
+                MorphStart = previousPosition + (visibilityRanges.[i] - previousPosition) * lodSettings.MorphStartRatio
+            }
+        lodRanges.[i] <- lodRange
+        previousPosition <- lodRange.MorphStart
     lodRanges
+//
+//      selectionObj->m_morphEnd[i] = selectionObj->m_visibilityRanges[index];
+//      selectionObj->m_morphStart[i] = prevPos + (selectionObj->m_morphEnd[i] - prevPos) * selectionObj->m_morphStartRatio;
+//
+//      prevPos = selectionObj->m_morphStart[i];
