f791ae35eede488dcebc793245abf66632198e3e
Chris Jansson <chris.janssom@gmail.com>
2015-07-30 16:51:24 +0200

    Allocate a flat mesh and upload it to opengl

---
+++ source/CjClutter.FSharp/CDLodOpenGl.fs
@@ -0,0 +1,24 @@
+﻿module CDLodOpenGl
+
+open OpenTK.Graphics.OpenGL4
+open CjClutter.OpenGl.EntityComponent
+open CjClutter.OpenGl.OpenGl.VertexTypes
+
+let makeSquareXZMesh dimension =
+    let mesh = MeshCreator.CreateXZGrid(dimension, dimension)
+    
+    let vertexBuffer = GL.GenBuffer()
+    let elementBuffer = GL.GenBuffer()
+
+    GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer)
+    let vertexDataSize = nativeint ((new Vertex3V3N()).SizeInBytes * mesh.Vertices.Length)
+    GL.BufferData(BufferTarget.ArrayBuffer, vertexDataSize, mesh.Vertices, BufferUsageHint.StaticDraw)
+    GL.BindBuffer(BufferTarget.ArrayBuffer, 0)
+
+    GL.BindBuffer(BufferTarget.ElementArrayBuffer, elementBuffer)
+    let elementDataSize = nativeint (sizeof<uint16> * mesh.Faces.Length * 3)
+
+    let elements = mesh.Faces |> Array.collect (fun f -> [| f.V0; f.V1; f.V2 |] ) |> Array.map (fun e -> uint16 e)
+    GL.BufferData(BufferTarget.ElementArrayBuffer, elementDataSize, elements, BufferUsageHint.StaticDraw)
+    GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0)
+    (vertexBuffer, elementBuffer)
